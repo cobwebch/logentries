e24730d9f654656f423756f380eb236b07b1f7aa
Francois Suter <francois@typo3.org>
2016-11-28 15:50:58 +0100

    Updated for TYPO3 7 LTS

--- Classes/Log/Logger.php
+++ Classes/Log/Logger.php
@@ -1,29 +1,21 @@
 <?php
 namespace Cobweb\LogEntries\Log;
 
-/***************************************************************
- * Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- * (c) 2013 Julien Henchoz <typo3@cobweb.ch>
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * All rights reserved
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class Logger
@@ -39,7 +31,7 @@ class Logger {
      */
     static public function emergency($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->emergency($process . ' - ' . $message);
         return $message;
@@ -53,7 +45,7 @@ class Logger {
      */
     static public function critical($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->critical($process . ' - ' . $message);
         return $message;
@@ -66,7 +58,7 @@ class Logger {
      */
     static public function alert($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->alert($process . ' - ' . $message);
         return $message;
@@ -79,7 +71,7 @@ class Logger {
      */
     static public function error($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->error($process . ' - ' . $message);
         return $message;
@@ -92,7 +84,7 @@ class Logger {
      */
     static public function warning($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->warning($process . ' - ' . $message);
         return $message;
@@ -105,7 +97,7 @@ class Logger {
      */
     static public function notice($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->notice($process . ' - ' . $message);
         return $message;
@@ -118,7 +110,7 @@ class Logger {
      */
     static public function info($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->info($process . ' - ' . $message);
         return $message;
@@ -131,10 +123,9 @@ class Logger {
      */
     static public function debug($process, $message) {
         /** @var \TYPO3\CMS\Core\Log\Logger $logger */
-        $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager')->getLogger(__CLASS__);
+        $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
         // Write to Log
         $logger->debug($process . ' - ' . $message);
         return $message;
     }
 }
-?>
--- Classes/Log/Writer/LogEntries.php
+++ Classes/Log/Writer/LogEntries.php
@@ -1,56 +1,45 @@
 <?php
 namespace Cobweb\LogEntries\Log\Writer;
 
-/***************************************************************
- * Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- * (c) 2013 Julien Henchoz <typo3@cobweb.ch>
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * All rights reserved
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
+use TYPO3\CMS\Core\Log\LogRecord;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Sends log messages to Logentries.com
- *
  */
 class LogEntries extends \TYPO3\CMS\Core\Log\Writer\AbstractWriter {
     /**
-     * Writes the log record to php://stderr
+     * Writes the log record to Logentries.
      *
-     * @param \TYPO3\CMS\Core\Log\LogRecord $record Log record
-     * @return \TYPO3\CMS\Core\Log\LogRecord
+     * @param LogRecord $record Log record
+     * @return LogRecord
      */
-    public function writeLog(\TYPO3\CMS\Core\Log\LogRecord $record) {
-        if (strstr(GeneralUtility::getHostname(), '.cobweb.blue')) {
+    public function writeLog(LogRecord $record) {
+        $hostName = GeneralUtility::getHostname();
+        if (strpos($hostName, '.cobweb.blue') !== false) {
             $context = 'STAGING';
-        } elseif (strstr(GeneralUtility::getHostname(), '.cobweb.dev')) {
+        } elseif (strpos($hostName, '.cobweb.dev') !== false) {
             $context = 'DEV';
         } else {
             $context = 'PROD';
         }
 
-        $log = include \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('logentries') . 'Lib/LeLogger/logentries.php';
+        $log = include ExtensionManagementUtility::extPath('logentries') . 'Lib/LeLogger/logentries.php';
         $log->log($context . ' - ' . $record->getMessage(), $record->getLevel());
         return $record;
     }
 }
-
-?>
